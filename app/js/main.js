e55b5c03fe16395053b0a68eafcea601b51c2096
Yevhen <webstyle.ugn@gmail.com>
2020-12-03 02:02:08 +0200

    added 404, about, contact

--- app/js/main.js
+++ app/js/main.js
@@ -11,7 +11,15 @@ $(function() {
          slidesToScroll: 4,
          arrows: false,
          dots: true,
-         
+      });
+
+      $('.product-one__tabs .tab').on('click', function(event) {
+        var id = $(this).attr('data-id');
+        $('.product-one__tabs').find('.tab-item').removeClass('active-tab').hide();
+        $('.product-one__tabs .tabs').find('.tab').removeClass('active');
+        $(this).addClass('active');
+        $('#' + id).addClass('active-tab').fadeIn();
+        return false;
       });
 
       $('.menu__btn').on('click', function() {
